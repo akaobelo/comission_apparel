0812ce0ca46bf0418455be9700956d6bf4e57a5b
Arven Jay Abelo <arven.abelo@itgroupinc.asia>
2026-08-12 10:00:01 +0800

    alpine warning

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;600;800;900&family=Inter:wght@400;500;600&display=swap" rel="stylesheet">
 
     <!-- Alpine.js -->
+    <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/collapse@3.x.x/dist/cdn.min.js"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <style>
         [x-cloak] { display: none !important; }
